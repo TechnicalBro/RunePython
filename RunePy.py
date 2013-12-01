cfec30dc7667a3198055695e3278c20f071b8866
Brandon <asydix@gmail.com>
2013-12-01 17:24:22 -0400

    Updating with working console formatting, valid naming schemes, and fully-functional item searching

--- RunePy.py
+++ RunePy.py
@@ -1,6 +1,8 @@
 import urllib2
 import urllib
 import re
+from texttable import Texttable
+from termcolor import colored
 
 def find_between(s, first, last):
     try:
@@ -26,64 +28,109 @@ class SearchItem:
     def get_item_change(self):
         return self.itemchange
 
+
 class GrandExhangeSearch:
+
     #Base url for all the item searches we'll do
-    _BASE_URL = "http://www.tip.it/runescape/grand-exchange-centre/search?itemsearch="
+    __BASE_URL__ = "http://www.tip.it/runescape/grand-exchange-centre/search?itemsearch="
+
     #Dataset of start and end search tags for our amount of results
-    _RESULTS_TAG = ['<p id="num_results">', '</p>']
+    __RESULTS_TAG__ = ['<p id="num_results">', '</p>']
+
     #Results Amount Tag
-    _RESULT_AMOUNT_TAG = ['<span>', '</span>']
+    __RESULT_AMOUNT_TAG__ = ['<span>', '</span>']
+
     #Dataset of start and end search tags for our table entries / item entires
-    _TABLE_TAG = ['<table id="gec_search_results"', '</table>']
+    __TABLE_TAG__ = ['<table id="gec_search_results"', '</table>']
 
-    _TABLE_ENTRY_REGEX = '<tr>.*?</tr>'
+    #Regex compile to match against all table rows in the HTML
+    __TABLE_ENTRY_REGEX__ = '<tr>.*?</tr>'
 
-    search_results = []
+    #Dataset to parse for the items id
+    __ITEM_ID_TAG__ = ['data-itemid="', '"']
 
-    search_results_count = 0
+    #Dataset to parse to get the items name
+    __ITEM_NAME_TAG__ = ['href="http://www.tip.it/runescape//grand-exchange-centre/view/[ITEMID]">', '</a>']
+
+    #Dataset to parse to get the items price
+    __ITEM_PRICE_TAG__ = ['[ITEMNAME]</a></td><td>', '</td><td>']
+
+    #Dataset to parse to get the items change
+    __ITEM_CHANGE_TAG__ = ['[ITEMPRICE]</td><td>', '</td><td><a href=']
 
     #Class initialization
-    def __init__(self, item):
+    def __init__(self, search_item):
         #Define our search item
-        self.item = item
+        self.search_item = search_item
         #Define our search URL
-        self.itemurl = self._BASE_URL + urllib.quote_plus(self.item)
-        print "Created a new search for " + item + " with url + " + self.itemurl
-        pagedata = self._getpagedata()
-        self.search_results_count = self._get_search_amount(pagedata)
-        print "There are " + str(self.search_results_count) + " search results"
-        self.search_results = self._get_search_entries(pagedata)
-        for sr in self.search_results:
-            print "Item " + sr.itemname + " has a price of " + sr.itemprice + " with a change of " + sr.itemchange
+        self.search_url = self.__BASE_URL__ + urllib.quote_plus(self.search_item)
+        pagedata = self.__getpagedata__()
+        self.search_results_count = self.__get_search_amount__(pagedata)
+        self.search_results = self.__get_search_entries__(pagedata)
 
-    def _get_search_amount(self, pagedata):
+    def __get_search_amount__(self, pagedata):
         #Find the data to parse to find our amount of results
-        searchresults = find_between(pagedata, self._RESULTS_TAG[0], self._RESULTS_TAG[1])
+        searchresults = find_between(pagedata, self.__RESULTS_TAG__[0], self.__RESULTS_TAG__[1])
         #Find the amount of results and format the commas in it
-        searchamount = find_between(searchresults, self._RESULT_AMOUNT_TAG[0], self._RESULT_AMOUNT_TAG[1])\
+        searchamount = find_between(searchresults, self.__RESULT_AMOUNT_TAG__[0], self.__RESULT_AMOUNT_TAG__[1])\
             .replace(",", "")
         return int(searchamount)
 
-    def _get_search_entries(self, pagedata):
+    def __get_search_entries__(self, pagedata):
+        #Create our list to return
         entries = []
-        table_data = find_between(pagedata, self._TABLE_TAG[0], self._TABLE_TAG[1])
-        re_entries = re.compile(self._TABLE_ENTRY_REGEX, re.IGNORECASE | re.DOTALL)
+        #Get the data for our table by parsing the strings between
+        table_data = find_between(pagedata, self.__TABLE_TAG__[0], self.__TABLE_TAG__[1])
+        #Match against table data to get all item entires
+        re_entries = re.compile(self.__TABLE_ENTRY_REGEX__, re.IGNORECASE | re.DOTALL)
+        #Loop through the matches to create our items
         for match in re_entries.findall(table_data):
-            item_id = find_between(match,'data-itemid="','"')
-            datamatch = '.*?(\w+).*?</td><td>(\S+)</td><td>'
-            print "Found item id for " + self.item + " to be " + item_id
-            item_regex = re.compile(datamatch)
-            data = item_regex.search(find_between(match, 'data-itemid="' + item_id + '"', '<a href=').replace(",", ""))
-            entries.append(SearchItem(data.group(0), data.group(1), data.group(2)))
+            #Find the item ID
+            item_id = find_between(match, self.__ITEM_ID_TAG__[0], self.__ITEM_ID_TAG__[1])
+            #Find the item name
+            item_name = find_between(match, self.__ITEM_NAME_TAG__[0].replace("[ITEMID]", item_id), self.__ITEM_NAME_TAG__[1])
+            #Find the item price
+            item_price = find_between(match, self.__ITEM_PRICE_TAG__[0].replace("[ITEMNAME]", item_name), self.__ITEM_PRICE_TAG__[1])
+            #Find the items change today
+            item_change = find_between(match, self.__ITEM_CHANGE_TAG__[0].replace("[ITEMPRICE]", item_price), self.__ITEM_CHANGE_TAG__[1])
+            #Append it to the data to return
+            entries.append(SearchItem(item_name, item_price, item_change))
         return entries
 
-    def _getpagedata(self):
-        response = urllib2.urlopen(self.itemurl)
+    def __getpagedata__(self):
+        response = urllib2.urlopen(self.search_url)
         return response.read()
+
+    def view_search_results(self):
+        table = Texttable(120)
+
+        table.set_cols_dtype(['t','t','t'])
+        table.set_cols_align(['c','c','c'])
+        table.add_row(["Item Name","Item Price", "Daily Change"])
+        for item in self.search_results:
+            itemchange_color = ""
+            if "+" in item.itemchange:
+                itemchange_color = "green"
+            elif "-" in item.itemchange:
+                itemchange_color = "red"
+            else:
+                itemchange_color = "yellow"
+            table.add_row([item.itemname, item.itemprice, colored(item.itemchange,itemchange_color)])
+        print table.draw()
 
 while True:
     itemsearch = raw_input("Please enter an item name: ")
     if itemsearch:
         geItem = GrandExhangeSearch(itemsearch)
-
-
+        while True:
+            viewitems = raw_input("There were " + str(geItem.search_results_count) + " results to your search, view them? [y/n]: ").lower()
+            if viewitems:
+                if viewitems == "y" or viewitems == "yes":
+                    geItem.view_search_results()
+                    break
+                elif viewitems == "n" or viewitems == "no":
+                    break
+            else:
+                print "Invalid option, please try again."
+    else:
+        break
